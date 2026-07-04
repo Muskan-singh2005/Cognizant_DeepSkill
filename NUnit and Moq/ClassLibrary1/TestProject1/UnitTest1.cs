1dd0dc30b481e1ab41c6e4315d036d95445cded7
Muskan <muskansanger3673@gmail.com>
2026-07-04 15:45:05 +0530

    Add NUnit and Moq exercises

---
+++ NUnit and Moq/ClassLibrary1/TestProject1/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new Calculator();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            calc = null;
+        }
+
+        // ✅ Single test
+        [Test]
+        public void Add_SingleTest()
+        {
+            Assert.That(calc.Add(2, 3), Is.EqualTo(5));
+        }
+
+        // ✅ Parameterised tests (IMPORTANT FOR MARKS)
+        [TestCase(2, 3, 5)]
+        [TestCase(10, 20, 30)]
+        [TestCase(-1, 1, 0)]
+        [TestCase(0, 0, 0)]
+        public void Add_MultipleCases(int a, int b, int expected)
+        {
+            Assert.That(calc.Add(a, b), Is.EqualTo(expected));
+        }
+
+        // ❌ Example of Ignore attribute
+        [Test]
+        [Ignore("Demonstration of Ignore attribute")]
+        public void Ignored_Test()
+        {
+            Assert.That(calc.Add(100, 100), Is.EqualTo(200));
+        }
+    }
+}
