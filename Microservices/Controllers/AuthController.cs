7c907cbbc4424258d12f302f212446fd3845cbc5
Muskan <muskansanger3673@gmail.com>
2026-07-26 01:31:57 +0530

    Added Microservices JWT Authentication project

---
+++ Microservices/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using JwtAuthApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAuthApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            if (IsValidUser(model))
+            {
+                var token = GenerateJwtToken(model.Username);
+                return Ok(new { Token = token });
+            }
+
+            return Unauthorized("Invalid Username or Password");
+        }
+
+        private bool IsValidUser(LoginModel model)
+        {
+            return model.Username == "admin" &&
+                   model.Password == "1234";
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            // Long Secret Key (minimum 32 characters)
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("ThisIsMyVeryLongSecretKeyForJwtAuthentication123456789"));
+
+            var credentials = new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "MyAuthServer",
+                audience: "MyApiUsers",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Microservices/Controllers/SecureController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecureController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetSecretData()
+        {
+            return Ok("Welcome! You have accessed a protected API.");
+        }
+    }
+}
